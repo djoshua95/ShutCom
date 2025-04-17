2da1ac35e6779ac0a63d7b1c1d0de83de3d24298
daniel <danielvillota1016@gmail.com>
2025-04-16 20:52:33 -0500

    Fix OrderByOptions.Selector property

--- src/backend/MysticMadness.Service/Generics/OrderByOptions.cs
+++ src/backend/MysticMadness.Service/Generics/OrderByOptions.cs
@@ -4,6 +4,6 @@ namespace MysticMadness.Service.Generics;
 
 public struct OrderByOptions<TEntity>
 {
-    public Expression<Func<TEntity, TEntity>> Selector { get; set; }
+    public Expression<Func<TEntity, object>> Selector { get; set; }
     public bool Descending { get; set; }
 }
